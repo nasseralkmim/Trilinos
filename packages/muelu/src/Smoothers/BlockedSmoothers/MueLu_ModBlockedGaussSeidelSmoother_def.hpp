f42c5168685aa2d656b673abf5849b7e33e6ef55
Nasser Alkmim <nasser.alkmim@gmail.com>
2025-03-05 12:42:31 +0100

    muelu: fix: fix calculation

--- packages/muelu/src/Smoothers/BlockedSmoothers/MueLu_ModBlockedGaussSeidelSmoother_def.hpp
+++ packages/muelu/src/Smoothers/BlockedSmoothers/MueLu_ModBlockedGaussSeidelSmoother_def.hpp
@@ -420,12 +420,17 @@ namespace MueLu {
           RCP<Matrix> F1_Hinv_C2 = MatrixFactory::BuildCopy(B2, false);
           Xpetra::MatrixMatrix<Scalar, LocalOrdinal, GlobalOrdinal, Node>::Multiply(*F1, false, *Hinv_C2, false, *F1_Hinv_C2, true, true);
           RCP<Matrix> Schur_B2 = MatrixFactory::BuildCopy(B2, false);
-          Xpetra::MatrixMatrix<Scalar, LocalOrdinal, GlobalOrdinal, Node>::TwoMatrixAdd(*F1_Hinv_C2, false, -one, *B2, false, one, Schur_B2, GetOStream(Statistics2));
+          Xpetra::MatrixMatrix<Scalar, LocalOrdinal, GlobalOrdinal, Node>::TwoMatrixAdd(*F1_Hinv_C2, false, -one, *B2, false, one, Schur_B2, this->GetOStream(Statistics2));
+
+          // Build a temporary RHS: temp_RHS = microrotation_RHS - Schur_B2 x_p1
+          RCP<MultiVector> temp_RHS = rangeMapExtractor_->getVector(1, rcpB->getNumVectors(), bRangeThyraMode);
+          Schur_B2->apply(*x_p1, *temp_RHS);
+          temp_RHS->update(one, *microrotation_RHS, -one);
           
           // TODO: this is a temporary solution, we need to implement a smoother for the Schur complement.
           // NOTE: using the smoother for D^{-1} instead of a smoother for the Schur
           // complement (D - F1 H^{-1} F2)^{-1}.
-          Inverse_.at(1)->Apply(*x_p2, *Schur_B2);
+          Inverse_.at(1)->Apply(*x_p2, *temp_RHS);
 
           // Store solution
           xhat1->update(one, *x_p1, zero);
